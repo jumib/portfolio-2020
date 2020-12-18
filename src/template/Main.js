6714a502fe97933d97661fad172a6e8c9b7af249
jumib <baekjumi@naver.com>
2020-12-18 23:49:20 +0900

    commit

--- src/template/Main.js
+++ src/template/Main.js
@@ -6,7 +6,8 @@ const Main = () => (
     <div>
     <h1>메인</h1>
     <p>포트폴리오 메인화면이 보여지는 공간입니다.</p>
-    <div image={require("assets/img/flistock1.jpg")}>
+    <div>
+    <img src={require("../assets/flistock.png")} />    
     </div>
     </div>
 )
