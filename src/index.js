d18a5484cba31a7138de7f274414e038f8cd2927
jumib <baekjumi@naver.com>
2023-11-07 17:04:00 +0900

    MOD: resolve package conflict

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-github-pages">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
